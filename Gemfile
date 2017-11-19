8fbf304c3f62567eefde4c8a22ff674238ec68e3
Caio <caio.dscamara@gmail.com>
2017-11-18 23:52:16 -0400

    Chatbot para RH.

---
+++ Gemfile
@@ -0,0 +1,33 @@
+source :rubygems
+
+# Sinatra
+gem 'sinatra'
+gem 'puma'
+gem 'rack-conneg'
+gem 'rake'
+gem 'sinatra-basic-auth'
+
+# Mongo + Mongoid
+gem 'mongo', '~> 2.1'
+gem 'mongoid'
+gem 'bson_ext'
+
+# Roar/Representable
+gem 'roar'
+gem 'multi_json'
+
+# Http Libs
+gem 'facebook-messenger'#,  :git => 'git://github.com/caiodsc/facebook_messenger.git'
+gem 'rest-client'
+
+group :development do
+ gem 'tux'
+end
+
+group :test do
+  #gem 'rack-test', require: 'rack/test'
+  gem 'rspec'
+  gem 'factory_girl', '~> 4.0'
+  gem 'ffaker'
+  #gem 'database_cleaner'
+end
---
+++ app.rb
@@ -0,0 +1,106 @@
+# Gems do sinatra e JSON.
+require 'json'
+require 'sinatra'
+require 'rest-client'
+require 'sinatra/basic_auth'
+
+
+# Pastas de configuração.
+require_relative './config.rb'
+require_relative './encrypt_decrypt.rb'
+
+# Configurando a gem facebook-messenger.
+require 'facebook/messenger'
+include Facebook::Messenger
+Facebook::Messenger::Subscriptions.subscribe(access_token: ACCESS_TOKEN)
+
+# Importando os diretórios.
+Dir["./app/models/*.rb"].each {|file| require file }
+Dir["./app/services/**/*.rb"].each {|file| require file }
+
+# Configurando o Mongoid.
+
+configure do
+  Mongoid.load!("config/mongoid.yml", settings.environment)
+  set :server, :puma # default to puma for performance
+end
+
+class App < Sinatra::Base
+  # Basic Auth for protect routes.
+  register Sinatra::BasicAuth
+  authorize do |username, password|
+    username == USER_AUTH && password == PASSWORD_AUTH
+  end
+
+  # Helpers Declaration
+  #helpers Sinatra::App::ApplicationHelper
+
+  # Specifying views and stylesheet directories
+  set :views, "./app/views"
+  set :public_folder, "./app/public"
+
+  # Routes
+  get '/' do
+    redirect url + 'info'
+  end
+
+  protect do
+    get '/info' do
+      #html :info
+      erb :info
+    end
+  end
+
+  post '/webhook' do
+    result = JSON.parse(request.body.read)["result"]
+    if result["contexts"].present?
+      response = InterpretService.call(result["contexts"][-2]["name"], result["action"], result["contexts"][-2]["parameters"], result["contexts"][-1]["parameters"]["facebook_sender_id"])
+    else
+      response = InterpretService.call(result["name"], result["action"], result["parameters"], result["parameters"]["facebook_sender_id"])
+    end
+    #response = result.to_s
+    content_type :json
+    response
+  end
+
+  get '/teste' do
+    #retorna
+    @treta = @params[:id].decrypt
+    erb :teste, :layout => :z_index
+  end
+
+  get '/contracheque' do
+    erb :contracheque, :layout => :z_index
+  end
+
+  get '/minhaavaliacao' do
+    erb :minhaavaliacao, :layout => :z_index
+  end
+
+  get '/certificado' do
+    erb :certificado, :layout => :z_index
+  end
+
+  get '/desconto' do
+    erb :desconto, :layout => :z_index
+  end
+
+  get '/bancohora' do
+    erb :bancohora, :layout => :z_index
+  end
+
+end
+
+
+#set :static_cache_control, [:public]
+#set :root, "./app"
+#response = result.to_s
+#content_type :json
+#{
+#    "speech": response,
+#    "displayText": response,
+#    "source": "OneBitBot"
+#}.to_json
+#def html(view)
+#  File.read(File.join('app/views', "#{view.to_s}.html"))
+#end
